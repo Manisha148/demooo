44f5a4bd0e17ac3df6d9d9df7ab4b31005d4b945
Manisha148 <70441355+Manisha148@users.noreply.github.com>
2023-05-11 17:56:25 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,3 +9,17 @@ variable "source_code_zip_file_key" {
 variable "codebuild_project_name" {
   type = string
 }
+variable "ecs_cluster_name" {
+  type    = string
+  default = "my-ecs-cluster"
+}
+
+variable "ecs_service_name" {
+  type    = string
+  default = "my-ecs-service"
+}
+
+variable "docker_image_tag" {
+  type    = string
+  default = "latest"
+}
