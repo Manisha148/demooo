58f59a5e3be3154f2cce80cd28e3b4f804258e16
Manisha148 <70441355+Manisha148@users.noreply.github.com>
2023-05-11 15:24:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,54 +1,90 @@
-terraform {
-  required_version = ">= 0.12"
-  # Uncomment only if you would like not to use s3 as backend
-  backend "s3" {
-    bucket = "my-tf-state-bucket-emeka"
-    key    = "terraform.tfstate"
-    region = "us-west-2"
+# Create the Elastic Beanstalk application and environment
+resource "aws_elastic_beanstalk_application" "example" {
+  name = "example"
+}
+
+resource "aws_elastic_beanstalk_environment" "example" {
+  name                = "example"
+  application         = aws_elastic_beanstalk_application.example.name
+  solution_stack_name = "64bit Amazon Linux 2 v5.4.10 running PHP 7.3"
+}
+
+# Create the CodePipeline
+resource "aws_codepipeline" "example" {
+  name = "example"
+
+  artifact_store {
+    location = "mybucketmanisha09845"
+    type     = "S3"
+  }
+
+  stage {
+    name = "Source"
+
+    action {
+      name            = "Source"
+      category        = "Source"
+      owner           = "AWS"
+      provider        = "CodeCommit"
+      version         = "1"
+      output_artifacts = ["source"]
+    }
+  }
+
+  stage {
+    name = "Build"
+
+    action {
+      name            = "Build"
+      category        = "Build"
+      owner           = "AWS"
+      provider        = "CodeBuild"
+      version         = "1"
+      input_artifacts = ["source"]
+      output_artifacts = ["build"]
+      configuration   = {
+        ProjectName = "example-build"
+        EnvironmentVariables = {
+          "APPLICATION_NAME" = aws_elastic_beanstalk_application.example.name
+          "ENVIRONMENT_NAME" = aws_elastic_beanstalk_environment.example.name
+        }
+      }
+    }
+  }
+
+  stage {
+    name = "Deploy"
+
+    action {
+      name            = "Deploy"
+      category        = "Deploy"
+      owner           = "AWS"
+      provider        = "ElasticBeanstalk"
+      version         = "1"
+      input_artifacts = ["build"]
+      configuration   = {
+        ApplicationName = aws_elastic_beanstalk_application.example.name
+        EnvironmentName = aws_elastic_beanstalk_environment.example.name
+      }
+    }
   }
 }
 
-provider "aws" {
-  region = var.aws_region
-}
-
-resource "aws_codecommit_repository" "code_repo" {
-  repository_name = "MyPythonApp"
-  description     = "This is a Sample python App Repository"
-}
-
-resource "null_resource" "image" {
-
-  provisioner "local-exec" {
-    command     = <<EOF
-       git init
-       git add .
-       git commit -m "Initial Commit"
-       git remote add origin ${aws_codecommit_repository.code_repo.clone_url_http}
-       git push -u origin master
-   EOF
-    working_dir = "showcase_flask_app" #"python_app"
+# Create the CodeBuild project
+resource "aws_codebuild_project" "example" {
+  name          = "example-build"
+  source {
+    type            = "CODECOMMIT"
+    location        = "my-repo"
+    git_clone_depth = 1
   }
-  depends_on = [
-    aws_codecommit_repository.code_repo,
-  ]
-
-}
-
-resource "null_resource" "clean_up" {
-
-  provisioner "local-exec" {
-    when        = destroy
-    command     = <<EOF
-       rm -rf .git/
-   EOF
-    working_dir = "showcase_flask_app" #"python_app"
-
+  artifacts {
+    type = "CODEPIPELINE"
   }
-}
-
-resource "aws_s3_bucket" "cicd_bucket" {
-  bucket        = var.artifacts_bucket_name
-  acl           = "private"
-  force_destroy = true
+  environment {
+    compute_type = "BUILD_GENERAL1_SMALL"
+    image        = "aws/codebuild/amazonlinux2-x86_64-standard:3.0"
+    type         = "LINUX_CONTAINER"
+  }
+  service_role = "arn:aws:iam::124288123671:role/awsrolecodebuld"
 }
