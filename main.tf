943ada3d7250a1151922bdc063d032d4284210f9
Manisha148 <70441355+Manisha148@users.noreply.github.com>
2023-05-12 17:22:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -93,3 +93,49 @@ resource "aws_codepipeline" "example" {
     }
   }
 }
+
+
+
+# resource "aws_codepipeline" "example" {
+#   name     = "example"
+#   role_arn = "arn:aws:iam::124288123671:role/awsrolecodebuld"
+
+#   artifact_store {
+#     location = "demopipeline00981"
+#     type     = "S3"
+#   }
+
+#   stage {
+#     name = "Source"
+
+#     action {
+#       name            = "Source"
+#       category        = "Source"
+#       owner           = "AWS"
+#       provider        = "S3"
+#       version         = "1"
+#       output_artifacts = ["source"]
+
+#       configuration = {
+#         S3Bucket    = var.s3_bucket_name
+#         S3ObjectKey = var.source_code_zip_file_key
+#       }
+#     }
+#   }
+
+#   stage {
+#     name = "Build"
+
+#     action {
+#       name            = "Build"
+#       category        = "Build"
+#       owner           = "AWS"
+#       provider        = "CodeBuild"
+#       version         = "1"
+#       input_artifacts  = ["source"]
+#       output_artifacts = ["build"]
+#       configuration   = {
+#         ProjectName      = var.codebuild_project_name
+#       }
+#     }
+#   }
