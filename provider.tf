1c042a5d9bdb4745e935e4b8521a8dd5b4d2df2f
Manisha148 <70441355+Manisha148@users.noreply.github.com>
2023-05-12 11:01:05 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,16 +9,5 @@ variable "source_code_zip_file_key" {
 variable "codebuild_project_name" {
   type = string
 }
-variable "codedeploy_application_name" {
-  type = string
-}
-
-variable "codedeploy_deployment_group_name" {
-  type = string
-}
-
-variable "codedeploy_deployment_config_name" {
-  type = string
-}
 
 
